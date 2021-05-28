8dfca3c93effbe45c0003e4981fdbb63ee6b5068
David-sqrtpi <david.stiven911@gmail.com>
2021-05-27 23:30:43 -0500

    Datepicker and submit button

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { PersonService } from './services/person.service';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +8,21 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder,
+    private personService: PersonService) { }
 
   personForm = this.fb.group({
     name: ['', Validators.required],
     id: ['', Validators.required],
     birthdate: ['', Validators.required]
   });
+
+  onSubmit() {
+    if (this.personForm.valid) {
+      this.personService.create(this.personForm.value).subscribe(
+        () => console.log(this.personForm.value),
+        err => console.error('Something went wrong ' + err)
+      );
+    }
+  }
 }
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,8 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatButtonModule } from '@angular/material/button';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
@@ -15,7 +22,13 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]

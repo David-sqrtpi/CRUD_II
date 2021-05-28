8ffb07823867fa9c66f6ac2b4e2e97f948e0a450
David-sqrtpi <56278983+David-sqrtpi@users.noreply.github.com>
2021-05-28 01:22:21 -0500

    Update person.java

--- java/src/main/java/com/david/java/Person.java
+++ java/src/main/java/com/david/java/Person.java
@@ -6,8 +6,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.sql.Date;
 
@@ -18,7 +16,6 @@ import java.sql.Date;
 @NoArgsConstructor
 public class Person {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String name;
     private Date birthday;
